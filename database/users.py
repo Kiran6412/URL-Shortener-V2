62cc55cd97602a854563f87aa4bf1eff3857caaf
Kiran6412 <136492278+Kiran6412@users.noreply.github.com>
2024-01-09 17:53:28 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -18,7 +18,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "tulinks.one",
+            "base_site": "tulinks.online",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
